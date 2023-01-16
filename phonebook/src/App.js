02f00ced7b967156b5d61064b97ab23c1e2ac9f7
Onayifeke Edison Oghenerunor <tkeddy5@gmail.com>
2023-01-16 14:24:18 +0100

    Phone book display component

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,68 +1,24 @@
 import { useState } from "react";
-import React from "react";
-import Note from "./components/Note.jsx";
 
-function App() {
- const [note, setNote] = useState([{ name: "eddy", id: 0 }]);
- const [newNote, setNewNote] = useState([...note]);
- const [impList, setImpList] = useState([...newNote]);
- console.log("Render");
-
- const addNote = (event) => {
-  event.preventDefault();
-  const name = document.getElementById("add-note").value;
-  const date = new Date().toISOString();
-  const id = newNote.length + 1;
-  const checkImportance = document.getElementById("imp");
-  const important = checkImportance.checked ? true : false;
-  let data = {
-   name: name,
-   date: date,
-   id: id,
-   important: important,
-  };
-  console.log(newNote);
-  notes.push(data);
-  setNewNote(newNote.concat(data));
-  showImp();
- };
-
- function showImp() {
-  const checkImp = document.getElementById("show-imp");
-  const filteredList = checkImp.checked
-   ? newNote.filter((el) => {
-      return el.important;
-     })
-   : newNote;
-
-  setImpList(filteredList);
- }
- let notes = [];
+const App = () => {
+ const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+ const [newName, setNewName] = useState("");
 
  return (
   <div>
-   <h1>NOTES APP</h1>
-   <form action="" onSubmit={addNote}>
-    <label htmlFor="add note">Add new note</label>
-    <br />
-    <input type="text" name="add note" id="add-note" />
-    <button type="submit">Submit</button>
-    <br />
-    <label htmlFor="">Important</label>
-    <input type="checkbox" name="important" id="imp" />
+   <h2>Phonebook</h2>
+   <form>
+    <div>
+     name: <input />
+    </div>
+    <div>
+     <button type="submit">add</button>
+    </div>
    </form>
-   <Note note={impList} />
-   <form action="">
-    <label htmlFor="">Show only important</label>
-    <input
-     type="checkbox"
-     name="show important"
-     id="show-imp"
-     onChange={showImp}
-    />
-   </form>
+   <h2>Numbers</h2>
+   <div>debug: {newName}</div>
   </div>
  );
-}
+};
 
 export default App;
