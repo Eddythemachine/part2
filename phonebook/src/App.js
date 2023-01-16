bb1af34e12bbab2477106b869c483dc3be969de7
Onayifeke Edison Oghenerunor <tkeddy5@gmail.com>
2023-01-16 15:17:59 +0100

    Adding the ability to add both numbers and id in the application

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,14 +1,20 @@
 import { useState } from "react";
 
 const App = () => {
- const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+ const [persons, setPersons] = useState([
+  { name: "Arto Hellas", no: "0807424244", id: 1 },
+ ]);
  const [newName, setNewName] = useState([...persons]);
 
  const savePhoneDetails = (event) => {
   event.preventDefault();
   const name = document.getElementById("name").value;
+  const no = document.getElementById("no").value;
+  const id = newName.length + 1;
   const data = {
    name: name,
+   no: no,
+   id: id,
   };
   let newData = [];
   newData = [...newName];
@@ -17,7 +23,7 @@ const App = () => {
    (el) => el.name.toLocaleLowerCase() === name.toLocaleLowerCase()
   );
   if (found) {
-   alert("Name exists in phone book");
+   alert(`${name} exists in phone book`);
   } else {
    setNewName(newData);
   }
@@ -31,6 +37,8 @@ const App = () => {
    <form onSubmit={savePhoneDetails}>
     <div>
      name: <input type="text" name="" id="name" />
+     <br />
+     number: <input type="number" name="" id="no" />
     </div>
     <div>
      <button type="submit">add</button>
