5e6c4f1abee5cd09129fe184d76e29c7ec6389d1
Onayifeke Edison Oghenerunor <tkeddy5@gmail.com>
2023-01-16 14:50:15 +0100

    Adding names to the phone book

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,21 +2,34 @@ import { useState } from "react";
 
 const App = () => {
  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
- const [newName, setNewName] = useState("");
+ const [newName, setNewName] = useState([...persons]);
 
+ const savePhoneDetails = (event) => {
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  console.log(name);
+  const data = {
+   name: name,
+  };
+  let newData = [];
+  newData = [...newName];
+  newData.push(data);
+  setNewName(newData);
+ };
+ console.log(newName);
  return (
   <div>
    <h2>Phonebook</h2>
-   <form>
+   <form onSubmit={savePhoneDetails}>
     <div>
-     name: <input />
+     name: <input type="text" name="" id="name" />
     </div>
     <div>
      <button type="submit">add</button>
     </div>
    </form>
    <h2>Numbers</h2>
-   <div>debug: {newName}</div>
+   {/* <div>debug: {newName}</div> */}
   </div>
  );
 };
